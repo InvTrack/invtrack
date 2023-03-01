7ff114837ea0113b8f8cfe56125e68854f9183ab
felix-lipski <felix.lipski7@gmail.com>
2023-03-01 22:31:45 +0000

    remove access keys

--- db/supebase.ts
+++ db/supebase.ts
@@ -1,8 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import keys from "./keys.json"
 
-const supabaseUrl = "https://pzwpgbnxlndlblmmcuoo.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6d3BnYm54bG5kbGJsbW1jdW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2ODk0NjIsImV4cCI6MTk5MzI2NTQ2Mn0.Kiid-moKg5BPa5IktFtbtn7ax69mxhU4VgW9RI-qcjs"
+const supabaseUrl = keys.supabaseUrl;
+const supabaseAnonKey = keys.supabaseAnonKey;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
