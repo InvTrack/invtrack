357adba0f9dcc818f52080f2d2c4403ce9980997
Sara Rekowska <sara.rekowska@dreamstormstudios.com>
2023-03-01 22:16:11 +0100

    feat: basic theme setup

--- theme/index.ts
+++ theme/index.ts
@@ -1,54 +1,48 @@
-import { DefaultTheme } from "@react-navigation/native";
-
 export type ThemeFontSize = typeof fontSize;
 const fontSize = {
-  xl: 0,
-  l: 0,
-  m: 0,
-  s: 0,
-  xs: 0,
+  xl: 40,
+  l: 24,
+  m: 22,
+  s: 20,
+  xs: 17,
 } as const;
 
 export type ThemeColors = keyof typeof themeColors;
 const themeColors = {
-  ...DefaultTheme.colors,
-  // all of these keys are required. change the values to hex?
-  primary: "rgb(0, 122, 255)",
-  background: "rgb(242, 242, 242)",
-  card: "rgb(255, 255, 255)",
-  text: "rgb(28, 28, 30)",
-  border: "rgb(216, 216, 216)",
-  notification: "rgb(255, 59, 48)",
+  black: "#000000",
+  white: "#FFFFFF",
+  lightBlue: "#EDF6FF",
+  mediumBlue: "#C9E0F6",
+  darkBlue: "#4A6D90",
 } as const;
 
 export type MainTheme = typeof mainTheme;
 export const mainTheme = {
-  ...DefaultTheme,
   dark: false,
   text: {
     xl: {
       fontSize: fontSize.xl,
-      lineHeight: 0,
+      lineHeight: 37,
       fontFamily: "",
     },
     l: {
       fontSize: fontSize.l,
-      lineHeight: 0,
+      lineHeight: 27,
       fontFamily: "",
     },
     m: {
       fontSize: fontSize.m,
-      lineHeight: 0,
+      lineHeight: 24,
       fontFamily: "",
     },
     s: {
       fontSize: fontSize.s,
-      lineHeight: 0,
+      lineHeight: 20,
       fontFamily: "",
     },
     xs: {
       fontSize: fontSize.xs,
-      lineHeight: 0,
+      lineHeight: 17,
       fontFamily: "",
     },
   },
