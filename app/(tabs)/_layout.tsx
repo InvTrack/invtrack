dd391ce5a3bcaf6eff0a34116eb93dd262bcc9f8
felix-lipski <felix.lipski7@gmail.com>
2023-03-01 20:24:25 +0000

    supabase: register and login

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -50,6 +50,20 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,14 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+// import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../db/supebase.ts'
+import Auth from '../components/Auth'
+import Account from '../components/Account'
+import { View } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -26,12 +32,27 @@ export default function RootLayout() {
     if (error) throw error;
   }, [error]);
 
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <>
-      {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
-      {!loaded && <SplashScreen />}
-      {loaded && <RootLayoutNav />}
-    </>
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+    </View>
+    // <>
+    //   {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
+    //   {!loaded && <SplashScreen />}
+    //   {loaded && <RootLayoutNav />}
+    // </>
   );
 }
 
