e9fa7ee303901ef0146c7b42ec79c708bec5462f
felix-lipski <felix.lipski7@gmail.com>
2023-03-03 18:53:31 +0000

    fix: improve useRecordPanel structure

---
+++ db/hooks/useUpdateRecord.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "react-query";
+import { supabase } from "../supabase";
+import { Record, RecordTable } from "../types";
+
+export const useUpdateRecord = (recordId: string) => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    async (record: Partial<Record>) => {
+      const { data, error } = await supabase
+        .from<"record", RecordTable>("record")
+        .update(record)
+        .eq("id", recordId)
+        .select()
+        .single();
+      if (error) throw new Error(error.message);
+      return data;
+    },
+    {
+      onMutate: async (record: Partial<Record>) => {
+        await queryClient.cancelQueries(["record", recordId]);
+        const previousRecord = queryClient.getQueryData<Record>([
+          "record",
+          recordId,
+        ]);
+        if (previousRecord?.quantity) {
+          queryClient.setQueryData(["record", recordId], {
+            ...previousRecord,
+            ...record,
+          });
+        }
+        return { previousRecord };
+      },
+      onSettled: () => queryClient.invalidateQueries(["record", recordId]),
+    }
+  );
+};
