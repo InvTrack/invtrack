efe96741576fe6f781f5ba41cd463f46c791d469
Michal Struck <michal.struck@lipsia.digital>
2023-03-16 21:50:32 +0100

    feat: textinputcontroller

---
+++ components/TextInputController/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  FieldValues,
+  UseControllerProps,
+  useController,
+} from "react-hook-form";
+
+import { TextInputProps, TextInput } from "../TextInput";
+// import { Typography } from "../Typography";
+
+interface TextInputControllerProps<T extends FieldValues>
+  extends UseControllerProps<T> {
+  textInputProps?: Omit<TextInputProps, "value" | "onChange">;
+}
+
+export const TextInputController = <T extends FieldValues>({
+  textInputProps,
+  ...props
+}: TextInputControllerProps<T>) => {
+  const {
+    field: { onChange, onBlur, ref, value },
+    // fieldState: { error },
+  } = useController(props);
+
+  // TODO Error handling
+  return (
+    <>
+      <TextInput
+        onChange={onChange}
+        onBlur={onBlur}
+        ref={ref}
+        value={value}
+        // error={!!error}
+        {...textInputProps}
+      />
+
+      {/* {error && <Typography variant="error">{error.message}</Typography>} */}
+    </>
+  );
+};
+
+export default TextInputController;
