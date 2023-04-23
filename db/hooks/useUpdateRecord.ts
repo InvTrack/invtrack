f70506cb93743794e4e5405b1611b4e34c1944b9
felix-lipski <felix.lipski7@gmail.com>
2023-04-23 20:18:28 +0000

    fix optimistic update

--- db/hooks/useUpdateRecord.ts
+++ db/hooks/useUpdateRecord.ts
@@ -15,23 +15,23 @@ export const useUpdateRecord = (recordId: string) => {
       if (error) throw new Error(error.message);
       return data;
     },
-    // TODO: fix optimistic update
     {
       onMutate: async (record: Partial<Record>) => {
-        await queryClient.cancelQueries(["record", recordId]);
+        await queryClient.cancelQueries(["product_record", recordId]);
         const previousRecord = queryClient.getQueryData<Record>([
-          "record",
+          "product_record",
           recordId,
         ]);
         if (previousRecord?.quantity) {
-          queryClient.setQueryData(["record", recordId], {
+          queryClient.setQueryData(["product_record", recordId], {
             ...previousRecord,
             ...record,
           });
         }
         return { previousRecord };
       },
-      onSettled: () => queryClient.invalidateQueries(["record", recordId]),
+      onSettled: () =>
+        queryClient.invalidateQueries(["product_record", recordId]),
     }
   );
 };
