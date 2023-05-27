3a07cfe97fbed4e1454b9424d69c6157bd9d631c
Felix Lipski <felix.lipski7@gmail.com>
2023-05-27 22:47:45 +0200

    Revert "revert monorepo restructure"
    This reverts commit 7877c2631dd2f3ded1cc7aadb56f27dd84ec9637.

--- native/db/supabase.ts
+++ native/db/supabase.ts
@@ -1,14 +1,13 @@
-import { Platform } from "react-native";
+import { Platform } from 'react-native'
 if (Platform.OS !== "web") {
-  require("react-native-url-polyfill/auto");
+  require('react-native-url-polyfill/auto')
 }
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 // import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-const localUrl = "http://localhost:54321";
-const localAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+const localUrl = "http://localhost:54321"
+const localAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 // const supabaseUrl = SUPABASE_URL;
 // const supabaseAnonKey = SUPABASE_ANON_KEY;
 
