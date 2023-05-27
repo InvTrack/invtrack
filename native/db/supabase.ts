f2b59ee6d22f87e10fdb342af9a6b5abc74d9cfc
Felix Lipski <felix.lipski7@gmail.com>
2023-05-27 21:36:34 +0200

    fixes

--- native/db/supabase.ts
+++ native/db/supabase.ts
@@ -1,3 +1,7 @@
+import { Platform } from 'react-native'
+if (Platform.OS !== "web") {
+  require('react-native-url-polyfill/auto')
+}
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
