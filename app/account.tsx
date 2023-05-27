979f58bc84efe8bc5c3cb350e28bb7acac5cb9ad
Felix Lipski <felix.lipski7@gmail.com>
2023-05-27 12:50:50 +0200

    ehh

--- app/account.tsx
+++ app/account.tsx
@@ -1,14 +1,22 @@
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useContext } from "react";
 import { View } from "react-native";
 import { Button } from "react-native-elements";
-import { supabase } from "../db";
+import { SessionContext, supabase } from "../db";
+// import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 export default function AccountDetails() {
   const router = useRouter();
+  const { googleAccessToken } = useContext(SessionContext);
 
   return (
     <View>
+      <Button
+        title="Google sheets"
+        onPress={async () => {
+          console.log(googleAccessToken);
+        }}
+      />
       <Button
         title="Sign Out"
         onPress={() => {
