1a30c4cdfca71979b57d84481b86a64af63d06b7
Felix Lipski <felix.lipski7@gmail.com>
2023-05-27 10:01:48 +0200

    dotenv setup

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [require.resolve("expo-router/babel")],
+    plugins: [
+      require.resolve("expo-router/babel"),
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
