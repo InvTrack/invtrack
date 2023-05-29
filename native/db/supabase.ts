081591a0f57674fd499135412cf989eb2e94f750
Michal Struck <michal.struck@lipsia.digital>
2023-05-29 21:26:06 +0200

    fix: kinda fix import sort

--- native/db/supabase.ts
+++ native/db/supabase.ts
@@ -1,13 +1,21 @@
+// organize-imports-ignore
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-import "react-native-url-polyfill/auto";
+import { Platform } from "react-native";
 
-import keys from "./.secrets/api-keys.json";
+if (Platform.OS !== "web") {
+  require("react-native-url-polyfill/auto");
+}
 
-const supabaseUrl = keys.supabaseUrl;
-const supabaseAnonKey = keys.supabaseAnonKey;
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+const localUrl = "http://localhost:54321";
+const localAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+// const supabaseUrl = SUPABASE_URL;
+// const supabaseAnonKey = SUPABASE_ANON_KEY;
+
+export const supabase = createClient(localUrl, localAnonKey, {
   auth: {
     storage: AsyncStorage as any,
     autoRefreshToken: true,
