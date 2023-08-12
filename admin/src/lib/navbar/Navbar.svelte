8cdd897ef186d2c7e80998b0f1c9bc9b5d145fdf
Felix Lipski <felix.lipski7@gmail.com>
2023-08-12 20:13:23 +0200

    better login

---
+++ admin/src/lib/navbar/Navbar.svelte
@@ -0,0 +1,42 @@
+<script>
+  import HomeIcon from "$lib/navbar/icons/HomeIcon.svelte";
+  import UsersIcon from "$lib/navbar/icons/UsersIcon.svelte";
+  import { supabase } from "$lib/supabase";
+  import NavbarItem from "./NavbarItem.svelte";
+  import ListIcon from "./icons/ListIcon.svelte";
+  import ProductIcon from "./icons/ProductIcon.svelte";
+  const handleLogout = () => supabase.auth.signOut();
+</script>
+
+<nav
+  id="default-sidebar"
+  class="w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+  aria-label="Sidebar"
+>
+  <div class="h-full flex flex-col px-3 py-4 overflow-y-auto bg-gray-800">
+    <ul class="flex-1 space-y-2 font-medium">
+      <NavbarItem path="/">
+        <HomeIcon />
+        <span class="flex-1 ml-3">Dashboard</span>
+      </NavbarItem>
+      <NavbarItem path="/workers">
+        <UsersIcon />
+        <span class="flex-1 ml-3 whitespace-nowrap">Workers</span>
+      </NavbarItem>
+      <NavbarItem path="/">
+        <ListIcon />
+        <span class="flex-1 ml-3 whitespace-nowrap">Inventories</span>
+      </NavbarItem>
+      <NavbarItem path="/">
+        <ProductIcon />
+        <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
+      </NavbarItem>
+    </ul>
+    <button
+      class="w-full flex items-center justify-start p-2 rounded-lg text-white hover:bg-gray-700 group"
+      on:click={handleLogout}
+    >
+      <span class="flex-1 whitespace-nowrap">Logout</span>
+    </button>
+  </div>
+</nav>
---
+++ admin/src/lib/navbar/NavbarItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  export let path: string;
+</script>
+
+<li aria-current={$page.url.pathname === path ? "page" : undefined}>
+  <a href={path} class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group">
+    <slot />
+  </a>
+</li>
