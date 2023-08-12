a8e31cccb357a483b821251b7c06b3904e205004
Felix Lipski <felix.lipski7@gmail.com>
2023-08-12 16:25:54 +0200

    workers list works

--- admin/src/routes/+layout.svelte
+++ admin/src/routes/+layout.svelte
@@ -1,6 +1,60 @@
-<script>
+<script lang="ts">
 	import Navbar from './Navbar.svelte';
 	import './styles.css';
+
+	import { onMount } from 'svelte';
+	import { supabase } from '../lib/supabase';
+	import type { AuthSession } from '@supabase/supabase-js';
+	import { page } from '$app/stores';
+	import { googleAccessToken } from '../lib/store';
+	// import Account from './lib/Account.svelte'
+	// import Auth from './lib/Auth.svelte'
+
+	let session: AuthSession;
+
+	onMount(() => {
+		// console.log($page)
+		const urlStringOriginal = $page.url.href;
+		if (urlStringOriginal?.includes('#access_token')) {
+			const urlString = urlStringOriginal.replace('#access_token', '?access_token');
+			const url = new URL(urlString);
+			const refreshToken = url.searchParams.get('refresh_token');
+			const accessToken = url.searchParams.get('access_token');
+			const providerToken = url.searchParams.get('provider_token');
+			console.log({
+				urlStringOriginal,
+				providerToken,
+				accessToken,
+				refreshToken
+			});
+			if (accessToken && refreshToken) {
+				supabase.auth
+					.setSession({
+						refresh_token: refreshToken,
+						access_token: accessToken
+					})
+					.then((res) => {
+						// console.log({ res });
+						// setSession(session);
+						res.data.session;
+					})
+					.catch((err) => console.log({ err }));
+			}
+			if (providerToken) {
+				console.log({ providerToken });
+				googleAccessToken.set(providerToken);
+			}
+		}
+
+		supabase.auth.getSession().then(({ data }) => {
+			console.log(data);
+			if (data.session) session = data.session;
+		});
+
+		supabase.auth.onAuthStateChange((_event, _session) => {
+			if (_session) session = _session;
+		});
+	});
 </script>
 
 <div class="app">
@@ -9,10 +63,6 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -27,7 +77,7 @@
 		display: flex;
 		flex-direction: column;
 		padding: 1rem;
-		width: 100%;
+		/* width: 100%; */
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
--- admin/src/routes/+page.svelte
+++ admin/src/routes/+page.svelte
@@ -4,7 +4,5 @@
 </svelte:head>
 
 <section>
-	<h1 class="text-3xl font-bold underline">
-	  Hello world!
-	</h1>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
 </section>
