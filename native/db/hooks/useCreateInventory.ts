161d15768012580f93da03011543d1223bc35345
Michal Struck <michal.struck@lipsia.digital>
2023-08-13 21:51:11 +0200

    fix: invalidate inventory list query after mutation

--- native/db/hooks/useCreateInventory.ts
+++ native/db/hooks/useCreateInventory.ts
@@ -1,12 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { supabase } from "../supabase";
 import { Inventory, InventoryTable } from "../types";
 import { useSession } from "./sessionContext";
 
 export const useCreateInventory = () => {
-  const { companyId } = useSession();
-
+  const { companyId, session } = useSession();
+  const queryClient = useQueryClient();
   return useMutation(
     async (inventory: Omit<Inventory, "created_at" | "company_id" | "id">) => {
       const { data, error } = await supabase
@@ -15,6 +15,10 @@ export const useCreateInventory = () => {
         .select();
       if (error) throw new Error(error.message);
       return data;
+    },
+    {
+      onSuccess: () =>
+        queryClient.invalidateQueries(["inventories", session?.user.id]),
     }
   );
 };
