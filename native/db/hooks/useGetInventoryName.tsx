6022df98a66fc324ecbbf2955d5442b5390bd1d0
Michal Struck <michal.struck@lipsia.digital>
2023-08-14 21:56:14 +0200

    refactor: simplify hook, remove unnecessary session dep

--- native/db/hooks/useGetInventoryName.tsx
+++ native/db/hooks/useGetInventoryName.tsx
@@ -2,25 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 
 import { supabase } from "../supabase";
 import { InventoryTable } from "../types";
-import { useSession } from "./sessionContext";
-
-const getInventoryName = async (inventoryId: number) => {
-  const res = await supabase
-    .from<"inventory", InventoryTable>("inventory")
-    .select("name")
-    .eq("id", inventoryId);
-  return {
-    ...res,
-    data: res.data?.[0].name,
-  };
-};
 
 export const useGetInventoryName = (inventoryId: number) => {
-  const { session } = useSession();
-
-  const query = useQuery(
-    ["inventoryName", inventoryId],
-    () => session && getInventoryName(inventoryId)
-  );
+  const query = useQuery(["inventoryName", inventoryId], async () => {
+    const res = await supabase
+      .from<"inventory", InventoryTable>("inventory")
+      .select("name")
+      .eq("id", inventoryId);
+    return {
+      ...res,
+      data: res.data?.[0].name,
+    };
+  });
   return { ...query, data: query.data?.data };
 };
