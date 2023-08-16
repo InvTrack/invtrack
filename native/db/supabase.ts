711a4ea96165dba782d11007c38854b6de44d4c0
Michal Struck <michal.struck@lipsia.digital>
2023-08-16 22:55:13 +0200

    redirect-unmatched-routes-and-create-a-custom-header and various small inset-related fixes

--- native/db/supabase.ts
+++ native/db/supabase.ts
@@ -9,7 +9,7 @@ if (Platform.OS !== "web") {
 
 // import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
 
-const localUrl = "http://localhost:54321";
+const localUrl = "http://172.20.10.12:54321";
 const localAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 // const supabaseUrl = SUPABASE_URL;
