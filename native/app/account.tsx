90a1d4c461aa263355d073b3ef2e32c70c25a82e
Michal Struck <michal.struck@lipsia.digital>
2023-08-20 21:16:10 +0200

    fix: simplify account screen, adjust cursor color

--- native/app/account.tsx
+++ native/app/account.tsx
@@ -6,71 +6,92 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "../components/Button";
 import TextInputController from "../components/TextInputController";
 
-import { StyleSheet } from "react-native";
-import { supabase, useGetUser, useUpdateUser } from "../db";
+import { ScrollView, StyleSheet } from "react-native";
+
+import { Typography } from "../components/Typography";
+import { supabase } from "../db";
 import { useSession } from "../db/hooks/sessionContext";
 import { createStyles } from "../theme/useStyles";
 
 export default function AccountDetails() {
   const styles = useStyles();
   const { session } = useSession();
-  const { data: user, isLoading } = useGetUser();
+  // const { data: user, isLoading } = useGetUser();
 
-  const updateUser = useUpdateUser();
+  // const updateUser = useUpdateUser();
   const router = useRouter();
   const { control } = useForm();
+
   return (
     <SafeAreaView edges={["left", "right", "bottom"]} style={styles.container}>
-      <TextInputController
-        control={control}
-        name="email"
-        textInputProps={{
-          placeholder: session?.user?.email,
-        }}
-      />
-      <TextInputController
-        control={control}
-        name="username"
-        textInputProps={{
-          placeholder: user?.username,
-          containerStyle: styles.mt,
-        }}
-      />
-      <TextInputController
-        control={control}
-        name="companyName"
-        textInputProps={{
-          placeholder: user?.company_name,
-          containerStyle: styles.mt,
-        }}
-      />
-      <Button
-        onPress={() =>
-          updateUser.mutate({
-            username: user?.username,
-            companyName: user?.company_name,
-          })
-        }
-        type="primary"
-        size="s"
-        fullWidth
-        disabled={isLoading}
-        containerStyle={styles.mt}
-      >
-        {isLoading ? "Ładowanie ..." : "Aktualizuj"}
-      </Button>
-      <Button
-        onPress={async () => {
-          await supabase.auth.signOut();
-          router.push("/(start)/start");
-        }}
-        type="secondary"
-        size="s"
-        fullWidth
-        containerStyle={styles.mt2}
-      >
-        Wyloguj się
-      </Button>
+      <ScrollView>
+        <Typography variant="xlBold" color="darkBlue" style={styles.mb}>
+          Ustawienia
+        </Typography>
+        <Typography variant="lBold" color="darkBlue" style={styles.mb}>
+          Twój email:
+        </Typography>
+        <TextInputController
+          control={control}
+          name="email"
+          textInputProps={{
+            placeholder: session?.user?.email,
+            disabled: true,
+            editable: false,
+            containerStyle: styles.mb,
+          }}
+        />
+
+        {/* <TextInputController
+          control={control}
+          name="username"
+          defaultValue={user?.username}
+          textInputProps={{
+            placeholder: user?.username,
+            containerStyle: styles.mt,
+            defaultValue: user?.username,
+          }}
+        />
+        <TextInputController
+          control={control}
+          name="companyName"
+          defaultValue={user?.company_name}
+          textInputProps={{
+            placeholder: user?.company_name,
+            containerStyle: styles.mt,
+            defaultValue: user?.company_name,
+          }}
+        />
+        <Button
+          // onPress={() =>
+          //   updateUser.mutate({
+          //     username: user?.username,
+          //     companyName: user?.company_name,
+          //   })
+          // }
+
+          type="primary"
+          size="s"
+          fullWidth
+          // until we figure out what to do here
+          disabled={true}
+          containerStyle={styles.mt}
+        >
+          {isLoading ? <LoadingSpinner /> : "Aktualizuj"}
+        </Button> */}
+        <Button
+          onPress={async () => {
+            await supabase.auth.signOut();
+            router.push("/(start)/start");
+          }}
+          type="secondary"
+          size="s"
+          fullWidth
+          containerStyle={styles.mt2}
+        >
+          Wyloguj się
+        </Button>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -83,12 +104,9 @@ const useStyles = createStyles((theme) =>
       alignItems: "center",
       height: "100%",
     },
-    updateButton: {},
-    mt: {
-      marginTop: theme.spacing * 2,
-    },
     mt2: {
       marginTop: theme.spacing * 9,
     },
+    mb: { marginBottom: 16 },
   })
 );
