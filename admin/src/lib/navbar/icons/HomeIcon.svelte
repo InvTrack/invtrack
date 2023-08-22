00ed5bbcdd69904d4aff030fc6e66b80f5aff8cd
Michal Struck <michal.struck@lipsia.digital>
2023-08-22 21:09:54 +0200

    feat: init flowbite + migrate navbar

--- admin/src/lib/navbar/icons/HomeIcon.svelte
+++ admin/src/lib/navbar/icons/HomeIcon.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+  export let twStyle = "w-6 h-6";
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   fill="none"
   viewBox="0 0 24 24"
   stroke-width="1.5"
   stroke="currentColor"
-  class="w-6 h-6"
+  class={twStyle}
 >
   <path
     stroke-linecap="round"
