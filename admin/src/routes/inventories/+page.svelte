53adcf780f91158d5d48f567179f3ce8d023b212
Michal Struck <michal.struck@lipsia.digital>
2023-08-26 20:34:12 +0200

    refactor: inventories + edit in flowbite, sidebar in pos fixed, screencard margin fix

--- admin/src/routes/inventories/+page.svelte
+++ admin/src/routes/inventories/+page.svelte
@@ -2,11 +2,10 @@
   import { onMount } from "svelte";
   import { supabase } from "$lib/supabase";
   import type { Tables } from "$lib/helpers";
-  import Head from "$lib/table/Head.svelte";
-  import Table from "$lib/table/Table.svelte";
-  import Row from "$lib/table/Row.svelte";
-  import Card from "$lib/main/Card.svelte";
+
   import { genericGet } from "$lib/genericGet";
+  import ScreenCard from "$lib/ScreenCard.svelte";
+  import { Table, TableBodyRow, TableHead } from "flowbite-svelte";
 
   let inventories: Tables<"inventory">[] | null = null;
   onMount(() =>
@@ -17,18 +16,18 @@
   );
 </script>
 
-<Card header="Inventories">
+<ScreenCard header="Inventories">
   {#if inventories}
     <Table>
-      <Head>
+      <TableHead>
         <th scope="col" class="px-6 py-3">Name</th>
         <th scope="col" class="px-6 py-3">Date</th>
         <th scope="col" class="px-6 py-3">Created at</th>
         <th scope="col" class="px-6 py-3" />
-      </Head>
+      </TableHead>
       <tbody>
         {#each inventories as inventory}
-          <Row>
+          <TableBodyRow>
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -47,9 +46,9 @@
                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a
               >
             </td>
-          </Row>
+          </TableBodyRow>
         {/each}
       </tbody>
     </Table>
   {/if}
-</Card>
+</ScreenCard>
