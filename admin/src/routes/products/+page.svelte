5113245ad9f9d2b2b2aa67e4855f06875368ad33
Michal Struck <michal.struck@lipsia.digital>
2023-08-27 14:59:38 +0200

    feat: step editing in products

--- admin/src/routes/products/+page.svelte
+++ admin/src/routes/products/+page.svelte
@@ -4,30 +4,23 @@
   import type { Tables } from "$lib/helpers";
 
   import { genericGet } from "$lib/genericGet";
-  import {
-    Card,
-    Heading,
-    Table,
-    TableBody,
-    TableBodyRow,
-    TableHead,
-    TableHeadCell,
-  } from "flowbite-svelte";
+  import { Table, TableBody, TableBodyRow, TableHead, TableHeadCell } from "flowbite-svelte";
   import ScreenCard from "$lib/ScreenCard.svelte";
   import { parseISODatestring } from "$lib/dates/parseISODatestring";
 
   let products: Tables<"product">[] | null = null;
-  onMount(() => genericGet(supabase.from("product").select(), (x) => (products = x)));
+  onMount(() => genericGet(supabase.from("product").select("*"), (x) => (products = x)));
 </script>
 
 <ScreenCard header="Products">
   {#if products}
     <Table striped>
       <TableHead class="dark:bg-gray-700 bg-gray-50">
-        <th scope="col" class="px-6 py-3">Name</th>
-        <th scope="col" class="px-6 py-3">Unit</th>
-        <th scope="col" class="px-6 py-3">Created at</th>
-        <th scope="col" class="px-6 py-3" />
+        <TableHeadCell scope="col" class="px-6 py-3">Name</TableHeadCell>
+        <TableHeadCell scope="col" class="px-6 py-3">Unit</TableHeadCell>
+        <TableHeadCell scope="col" class="px-6 py-3">Created at</TableHeadCell>
+        <TableHeadCell scope="col" class="px-6 py-3">Steps</TableHeadCell>
+        <TableHeadCell scope="col" class="px-6 py-3" />
       </TableHead>
       <TableBody>
         {#each products as product}
@@ -40,6 +33,9 @@
             </td>
             <td class="px-6 py-4">
               {parseISODatestring(product.created_at)}
+            </td>
+            <td class="px-6 py-4">
+              {product.steps.map((step) => " " + step)}
             </td>
             <td class="px-6 py-4 text-right">
               <a
