cde8a402f253644f1c1d4dcd8e5940a3109b2bb1
Michal Struck <michal.struck@lipsia.digital>
2023-08-27 15:34:19 +0200

    create take 2

--- admin/src/routes/workers/+page.svelte
+++ admin/src/routes/workers/+page.svelte
@@ -29,19 +29,17 @@
   let name: Tables<"worker">["name"] = null;
   let is_admin: Tables<"worker">["is_admin"] = false;
   supabase.auth.getUser().then((x) => console.log(x));
-  const update = () => {};
-  // genericUpdate((
+  const update = () => {}; // genericUpdate(
   //   supabase
   //     .from("worker")
   //     .insert({
   //       is_admin,
   //       name,
-  //       company_id,
+  //       company_id
   //     })
-  //     .eq("company_id", company_id),
   //   "/workers",
   //   (x) => (loading = x)
-  // );)
+  // );
 </script>
 
 <ScreenCard header="Workers">
