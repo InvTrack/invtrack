b4268460538e87bbe00ad6d9aa8f5961aa4f0eae
Michal Struck <michal.struck@lipsia.digital>
2023-10-14 11:46:34 +0200

    fix: format

--- admin/src/lib/genericGet.ts
+++ admin/src/lib/genericGet.ts
@@ -6,13 +6,13 @@ export const genericGet = async <T>(
   setLoading?: (x: boolean) => void
 ) => {
   try {
-    setLoading && setLoading(true)
+    setLoading && setLoading(true);
     const { data, error, status, count } = await builder;
     if (error && status !== 406) throw error;
     if (data) setter(data, count);
   } catch (error) {
     if (error instanceof Error) alert(error.message);
   } finally {
-    setLoading && setLoading(false)
+    setLoading && setLoading(false);
   }
 };
--- admin/src/lib/genericUpdate.ts
+++ admin/src/lib/genericUpdate.ts
@@ -1,18 +1,21 @@
 import { goto } from "$app/navigation";
 import type { PostgrestBuilder } from "@supabase/postgrest-js";
 
-
-export const genericUpdate = async <T>(builder: PostgrestBuilder<T>, onSuccess: string, setLoading?: (x: boolean) => void) => {
-    try {
+export const genericUpdate = async <T>(
+  builder: PostgrestBuilder<T>,
+  onSuccess: string,
+  setLoading?: (x: boolean) => void
+) => {
+  try {
     //   loading = true;
-      setLoading && setLoading(true);
-      let { error } = await builder;
-      if (error) throw error;
-      goto(onSuccess)
-    } catch (error) {
-      if (error instanceof Error) alert(error.message);
-    } finally {
-      setLoading && setLoading(false);
+    setLoading && setLoading(true);
+    let { error } = await builder;
+    if (error) throw error;
+    goto(onSuccess);
+  } catch (error) {
+    if (error instanceof Error) alert(error.message);
+  } finally {
+    setLoading && setLoading(false);
     //   loading = false;
-    }
   }
+};
