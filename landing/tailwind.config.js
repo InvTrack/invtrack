b18e8ae6c77fa7805001919a3691131f844a0d71
Michal Struck <michal.struck@lipsia.digital>
2023-10-26 22:58:45 +0200

    landing/first form take, new imgs, stuff as well

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Inter",
+        serif: "Inter",
+        mono: "Inter",
+      },
+      fontSize: {
+        "2xl": "4.5rem",
+        xl: "4rem",
+        lg: "3.38rem",
+        md: "2.25rem",
+        s: "1.75rem",
+        base: "1rem",
+      },
+    },
   },
   plugins: [],
 };
