987c4475f16102bb8bf7edb1cc1e086bf296f925
Michal Struck <michal.struck@lipsia.digital>
2023-11-06 20:50:57 +0100

    csv-export/use company_id to match

--- supabase/functions/csv-export/index.ts
+++ supabase/functions/csv-export/index.ts
@@ -4,51 +4,62 @@
 
 import * as postgres from "postgres";
 import Papa from "papaparse";
-
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-};
+import { corsHeaders } from "../_shared/cors.ts";
 
 const databaseUrl = Deno.env.get("SUPABASE_DB_URL")!;
 
 Deno.serve(async (req) => {
-  // This is needed if you're planning to invoke your function from a browser.
+  // preflight request
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
 
-  const pool = new postgres.Pool(databaseUrl, 3, true);
+  const { company_id } = await req.json();
+
+  if (typeof company_id !== "number") {
+    return new Response(
+      JSON.stringify({ error: "company_id must be a number" }),
+      {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 400,
+      }
+    );
+  }
+
   try {
-    const connection = await pool.connect();
-    // TODO Match by company_id
-    const result = await connection.queryObject`
+    const pool = new postgres.Pool(databaseUrl, 3, true);
+    try {
+      const connection = await pool.connect();
+
+      const result = await connection.queryObject`
     SELECT inventory.date, record_view.*
     FROM inventory
     JOIN record_view ON inventory.id = record_view.inventory_id
-    WHERE inventory.company_id = ${2}
-    ORDER BY inventory.date
-    LIMIT 10 OFFSET 0;`;
+    WHERE inventory.company_id = ${company_id}
+    ORDER BY inventory.date`;
 
-    const contents = Papa.unparse(result.rows);
+      const contents = Papa.unparse(result.rows);
 
-    return new Response(new Blob([contents]), {
-      headers: {
-        ...corsHeaders,
-        "Content-Type": "text/csv",
-        "Content-Disposition": "attachment; filename=inventory.csv",
-      },
-      status: 200,
-    });
+      return new Response(new Blob([contents]), {
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "text/csv",
+          "Content-Disposition": "attachment; filename=inventory.csv",
+        },
+        status: 200,
+      });
+    } catch (error) {
+      console.error(error);
+      return new Response(JSON.stringify({ error: error.message }), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 400,
+      });
+    } finally {
+      await pool.end();
+    }
   } catch (error) {
     console.error(error);
-
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { ...corsHeaders, "Content-Type": "application/json" },
-      status: 400,
-    });
-  } finally {
-    await pool.end();
+    return new Response(String(error?.message ?? error), { status: 500 });
   }
 });
 
@@ -56,3 +67,4 @@ Deno.serve(async (req) => {
 // curl -v 'http://127.0.0.1:54321/functions/v1/csv-export' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --output 'csv-export-test.csv' \
+//   --data '{"company_id":2}'
