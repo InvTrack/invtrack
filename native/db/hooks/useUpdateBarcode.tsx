03017147b78a3d10afcfe1119b34be414ff61ae8
Michal Struck <michal.struck@lipsia.digital>
2023-12-06 23:33:35 +0100

    feat: useListBarcodes more robust, update barcode

---
+++ native/db/hooks/useUpdateBarcode.tsx
@@ -0,0 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Alert } from "react-native";
+import { supabase } from "../supabase";
+
+interface InsertBarcodeParams {
+  new_barcode: string;
+  product_id: number;
+}
+
+const insertBarcode = async ({
+  new_barcode,
+  product_id,
+}: InsertBarcodeParams) => {
+  const { data, error } = await supabase.rpc("insert_barcode", {
+    new_barcode,
+    product_id,
+  });
+  console.log("useUpdateBarcode", data, error);
+  if (error) {
+    Alert.alert(
+      "Nie udało się dodać kodu kreskowego. Jest już przypisany do innego produktu."
+    );
+  }
+
+  return data;
+};
+
+export const useInsertBarcode = (inventory_id: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation(insertBarcode, {
+    cacheTime: 0,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["barcodeList", inventory_id]);
+    },
+  });
+};
