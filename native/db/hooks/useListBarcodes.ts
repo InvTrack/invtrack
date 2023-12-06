a2abd7b50dc62dc6fce7f860b1e5335d9667bec1
Michal Struck <michal.struck@lipsia.digital>
2023-12-06 23:33:35 +0100

    feat: useListBarcodes more robust, update barcode

--- native/db/hooks/useListBarcodes.ts
+++ native/db/hooks/useListBarcodes.ts
@@ -2,23 +2,33 @@ import { useQuery } from "@tanstack/react-query";
 
 import isEmpty from "lodash/isEmpty";
 import { supabase } from "../supabase";
-import { Product, Record } from "../types";
+import { Product, RecordView } from "../types";
 
 type ListBarcodePostgrestRes =
   | {
       barcodes: Product["barcodes"];
-      record_view: [{ id: Record["id"] } | null];
+      record_view: [
+        {
+          id: RecordView["id"];
+          name: RecordView["name"];
+          product_id: RecordView["product_id"];
+        } | null
+      ];
     }[]
   | null;
 
-type ListBarcodeReturn = {
-  [barcode: Product["barcodes"][number]]: Record["id"];
+export type ListBarcodeReturn = {
+  [barcode: Product["barcodes"][number]]: {
+    recordId: RecordView["id"];
+    recordName: RecordView["name"];
+    productId: RecordView["product_id"];
+  };
 };
 
 const listBarcodes = async (inventory_id: number) => {
   const res = await supabase
     .from("product")
-    .select("barcodes, record_view(id)")
+    .select("barcodes, record_view(id, name, product_id)")
     .eq("record_view.inventory_id", inventory_id);
 
   const data = res.data as ListBarcodePostgrestRes;
@@ -27,9 +37,11 @@ const listBarcodes = async (inventory_id: number) => {
   const reducedData = data.reduce((result, item) => {
     const barcodes = item.barcodes;
     const recordId = item.record_view?.[0]?.id;
-    if (!recordId) return result;
+    const recordName = item.record_view?.[0]?.name;
+    const productId = item.record_view?.[0]?.product_id;
+    if (!recordId || !recordName || !productId) return result;
     barcodes.forEach((barcode) => {
-      result[barcode] = recordId;
+      result[barcode] = { recordId, recordName, productId };
     });
     return result;
   }, {} as ListBarcodeReturn);
@@ -42,7 +54,7 @@ const listBarcodes = async (inventory_id: number) => {
   };
 };
 
-export const useListBarcodes = ({ inventoryId }: { inventoryId: number }) => {
+export const useListBarcodes = (inventoryId: number) => {
   const query = useQuery(
     ["listBarcodes", inventoryId],
     async () => await listBarcodes(inventoryId)
