ecf58fe9023156e70a6209d57f58d28f064d6b54
Michal Struck <michal.struck@lipsia.digital>
2023-12-06 23:34:09 +0100

    feat: migration adjusted, generated types

--- supabase/migrations/20231119131615_barcodes.sql
+++ supabase/migrations/20231119131615_barcodes.sql
@@ -1,23 +1,41 @@
 ALTER TABLE "public"."product" ADD COLUMN "barcodes" text[] DEFAULT '{}'::text[] NOT NULL;
 
-CREATE OR REPLACE FUNCTION check_unique_barcodes()
+CREATE OR REPLACE FUNCTION public.check_unique_barcodes()
 RETURNS TRIGGER AS $$
+DECLARE
+    new_barcode TEXT;
+    existing_barcode TEXT;
 BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM "public"."product" p
-    WHERE p.company_id = NEW.company_id
-      AND (
-        (ARRAY(SELECT unnest(NEW.barcodes)) && ARRAY(SELECT unnest(p.barcodes))) OR
-        (NEW.barcodes IS NULL AND p.barcodes IS NULL)
-      )
-  ) THEN
-    RAISE EXCEPTION 'Barcode must be unique within the same company_id';
-  END IF;
-  RETURN NEW;
+    -- Loop through each barcode in the NEW.barcodes array
+    FOREACH new_barcode IN ARRAY NEW.barcodes
+    LOOP
+        -- Check if the new barcode already exists in any other row with the same company_id
+        IF EXISTS (SELECT 1 FROM public.product
+                   WHERE company_id = NEW.company_id
+                   AND new_barcode = ANY (barcodes)
+                   AND NOT (id = NEW.id AND new_barcode = ANY(barcodes))) THEN
+            RAISE EXCEPTION 'Barcode % already exists for the same company', new_barcode;
+        END IF;
+    END LOOP;
+
+    -- Barcode is unique, allow the insert/update
+    RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 CREATE TRIGGER prevent_duplicate_barcodes
-BEFORE INSERT OR UPDATE ON "public"."product"
+BEFORE INSERT OR UPDATE OF barcodes ON "public"."product"
 FOR EACH ROW EXECUTE FUNCTION check_unique_barcodes();
+
+CREATE OR REPLACE FUNCTION insert_barcode(product_id INT, new_barcode TEXT) RETURNS TABLE (updated_product_id INT, updated_barcodes TEXT[]) AS
+$$
+BEGIN
+  UPDATE product
+  SET barcodes = barcodes || new_barcode
+  WHERE id = product_id
+  RETURNING id, barcodes INTO updated_product_id, updated_barcodes;
+
+  RETURN NEXT;
+END;
+$$ LANGUAGE plpgsql;
