45754ca83df626d2feab53e6f613ba55b2c4e8db
Michal Struck <michal.struck@lipsia.digital>
2024-01-18 22:44:01 +0100

    low-quantity-notifications/refactor: hooks

--- native/db/hooks/useUpdateRecord.ts
+++ native/db/hooks/useUpdateRecord.ts
@@ -2,37 +2,34 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { supabase } from "../supabase";
 import { Record, RecordTable } from "../types";
-
+const updateRecord = async (record: Partial<Record>, recordId: number) => {
+  const { data, error } = await supabase
+    .from<"product_record", RecordTable>("product_record")
+    .update(record)
+    .eq("id", recordId)
+    .select()
+    .single();
+  if (error) throw new Error(error.message);
+  return data;
+};
 export const useUpdateRecord = (recordId: number) => {
   const queryClient = useQueryClient();
-  return useMutation(
-    async (record: Partial<Record>) => {
-      const { data, error } = await supabase
-        .from<"product_record", RecordTable>("product_record")
-        .update(record)
-        .eq("id", recordId)
-        .select()
-        .single();
-      if (error) throw new Error(error.message);
-      return data;
+  return useMutation((record) => updateRecord(record, recordId), {
+    onMutate: async (record: Partial<Record>) => {
+      await queryClient.cancelQueries(["product_record", recordId]);
+      const previousRecord = queryClient.getQueryData<Record>([
+        "product_record",
+        recordId,
+      ]);
+      if (previousRecord?.quantity) {
+        queryClient.setQueryData(["product_record", recordId], {
+          ...previousRecord,
+          ...record,
+        });
+      }
+      return { previousRecord };
     },
-    {
-      onMutate: async (record: Partial<Record>) => {
-        await queryClient.cancelQueries(["product_record", recordId]);
-        const previousRecord = queryClient.getQueryData<Record>([
-          "product_record",
-          recordId,
-        ]);
-        if (previousRecord?.quantity) {
-          queryClient.setQueryData(["product_record", recordId], {
-            ...previousRecord,
-            ...record,
-          });
-        }
-        return { previousRecord };
-      },
-      onSettled: () =>
-        queryClient.invalidateQueries(["product_record", recordId]),
-    }
-  );
+    onSettled: () =>
+      queryClient.invalidateQueries(["product_record", recordId]),
+  });
 };
