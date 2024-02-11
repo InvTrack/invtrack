6ff3619aef8eba4a17523f2f0a1e13df1399f737
Michal Struck <michal.struck@lipsia.digital>
2024-02-11 15:17:41 +0100

    feat: first try - eas envs setup

---
+++ native/env.config.js
@@ -0,0 +1,29 @@
+import * as Updates from "expo-updates";
+
+// has to be a .js file
+
+// by default expo-updates takes the local .env file and uses it in update
+// so let's prevent that
+// https://docs.expo.dev/build-reference/variables/
+
+export let AppConfig = {
+  env: process.env?.EXPO_ENV || process.env.NODE_ENV,
+  supabaseUrl: process.env?.SUPABASE_URL || "http://172.20.10.14:54321",
+  supabaseAnonKey:
+    process.env?.SUPABASE_ANON_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+};
+
+if (Updates.channel === "production") {
+  AppConfig.supabaseUrl =
+    process.env?.SUPABASE_URL || "https://krureybgsibclbmlcyff.supabase.co";
+  AppConfig.supabaseAnonKey =
+    process.env?.SUPABASE_ANON_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZza2ZuaWhlamdnZ2ppYm9saHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUyMjM4ODksImV4cCI6MjAwMDc5OTg4OX0.MucBZoohSbpCJVIBFXE_qUbUu3fIuvqoljY0oXNwR68";
+} else if (Updates.channel === "staging") {
+  AppConfig.supabaseUrl =
+    process.env?.SUPABASE_URL || "https://vskfnihejgggjibolhzv.supabase.co";
+  AppConfig.supabaseAnonKey =
+    process.env?.SUPABASE_ANON_KEY ||
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydXJleWJnc2liY2xibWxjeWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUyNzE1NzIsImV4cCI6MjAwMDg0NzU3Mn0.PZ5et3XYm7_9bzUqhdxyMklrh1AFcNm3kT_3Vy-qz6w";
+}
