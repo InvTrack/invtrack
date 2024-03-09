5b94d566878f920dfeaddddefbf4e5e915858fde
Felix Lipski <felix.lipski7@gmail.com>
2024-03-09 15:20:35 +0100

    dnd init

--- admin/src/routes/(authenticated)/products/+page.ts
+++ admin/src/routes/(authenticated)/products/+page.ts
@@ -2,7 +2,19 @@ export const load = async ({ parent }) => {
   const { supabase } = await parent();
   const { data: products } = await supabase.from("product").select("*");
 
+  const uncategorisedProducts = products.filter(p => !p.category)
+
+  const { data: productCategories } = await supabase
+    .from("product_category")
+    .select(`
+      *,
+      product(*)
+    `);
+
+  console.log(productCategories, uncategorisedProducts)
+
   return {
-    products,
+    productCategories,
+    products, uncategorisedProducts
   };
 };
