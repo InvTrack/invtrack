dc0ddae24c8ef36c2e1c2f8a0855043216771372
Michal Struck <michal.struck@lipsia.digital>
2024-03-10 19:24:41 +0100

    app/fix: apple app store requirements fix

---
+++ native/components/PrivacyPolicy.tsx
@@ -0,0 +1,17 @@
+import { Linking, TouchableOpacity } from "react-native";
+import { Typography } from "./Typography";
+
+export const PrivacyPolicy = () => {
+  return (
+    <TouchableOpacity
+      onPress={() =>
+        Linking.openURL("https://invtrack.app/polityka-prywatnosci")
+      }
+      style={{ alignSelf: "center", marginTop: 64 }}
+    >
+      <Typography variant="m" color="darkGrey">
+        Polityka prywatności
+      </Typography>
+    </TouchableOpacity>
+  );
+};
