32fb81cd0315bf7380258ec7c1436b6edf44982e
felix-lipski <63930662+felix-lipski@users.noreply.github.com>
2024-03-16 13:14:41 +0100

    admin: proper empty category (#147)
    * remove dummy empty category
    
    * display categories in products table
    
    * uncategorised products on top

--- admin/src/routes/(authenticated)/inventories/+page.ts
+++ admin/src/routes/(authenticated)/inventories/+page.ts
@@ -1,4 +1,6 @@
-export const load = async ({ parent }) => {
+import type { LoadFunctionArgument } from "$lib/helpers";
+
+export const load = async ({ parent }: LoadFunctionArgument) => {
   const { supabase } = await parent();
   const { data: inventories } = await supabase
     .from("inventory")
