2ed8d90434049f22ab4716615ed774182bc9b27d
Felix Lipski <felix.lipski7@gmail.com>
2024-04-02 15:04:05 +0200

    adding recipes

--- admin/src/routes/(authenticated)/recipes/add/+page.ts
+++ admin/src/routes/(authenticated)/recipes/add/+page.ts
@@ -2,29 +2,17 @@ import { error } from "@sveltejs/kit";
 export const load = async ({ parent, params }) => {
   const id = params.id;
   const { supabase } = await parent();
-  const { data: recipe, error: supabaseError } = await supabase
-    .from("recipe")
-    .select(`
-      *,
-      recipe_part(*)
-    `)
-    .eq("id", id)
-    .single();
 
   const { data: products, error: supabaseError2 } = await supabase
     .from("product")
     .select();
 
-  if (supabaseError) {
-    console.error(supabaseError);
-    throw error(404, "Recipe not found.");
-  }
   if (supabaseError2) {
     console.error(supabaseError2);
     throw error(404, "Products not found.");
   }
   return {
-    recipe,
+    supabase,
     products
   };
 };
