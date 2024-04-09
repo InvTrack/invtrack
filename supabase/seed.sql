f9e77efd3db528548cd88996ca25fcd16541c7a2
Michal Struck <michal.o.struck@gmail.com>
2024-04-09 22:44:36 +0200

    update seed and scripts

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -15,46 +15,50 @@ update public.worker set name = 'Adam', company_id = 1, is_admin = true where id
 
 insert into public.product_category (name, company_id, display_order) values 
     ('Mięsa', 1, 0), 
-    ('Owoce', 1, 1);
+    ('Spody do pizzy', 1, 1);
 
 insert into public.product (name, unit, category_id, company_id, display_order) values
-    ('Szynka',   'kg',   1,    1, 0), 
-    ('Kurczak',  'kg',   1,    1, 1), 
-    ('Jabłka',   'kg',   2,    1, 0), 
-    ('Banany',   'kg',   2,    1, 1), 
-    ('Arbuzy',   'kg',   2,    1, 2), 
+    ('Salami Ventricina',       'kg',    1,    1, 0), 
+    ('Mąka Semola Rimacinata',  'worki', 1,    1, 1), 
+    ('Krem Truflowy 3%-sos',    'kg',    2,    1, 0), 
+    ('SPIANATA Pikantna',       'kg',    2,    1, 1), 
     ('Łyżeczki', 'szt.', NULL, 1, 0),
     ('Cukier',   'kg',   NULL, 1, 1);
 
 
 -- Inwentaryzacje
-insert into public.inventory (name, date, company_id)
-    values 
-      ('Testowa Inwentaryzacja 1',  '2023-02-02 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 2',  '2023-02-03 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 3',  '2023-02-04 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 4',  '2023-02-05 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 5',  '2023-02-06 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 6',  '2023-02-07 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 7',  '2023-02-08 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 8',  '2023-02-09 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 9',  '2023-02-10 00:00:00+00', 1),
-      ('Testowa Inwentaryzacja 10', '2023-02-11 00:00:00+00', 1);
+insert into public.inventory (name, date, company_id, is_delivery)
+    values
+      ('Inwentaryzacja 02-07',  '2023-02-07 00:00:00+00', 1, false),
+      ('Dostawa 02-07',         '2023-02-07 00:00:00+00', 1, true),
+      ('Inwentaryzacja 02-08',  '2023-02-08 00:00:00+00', 1, false),
+      ('Dostawa 02-08',         '2023-02-08 00:00:00+00', 1, true),
+      ('Inwentaryzacja 02-09',  '2023-02-09 00:00:00+00', 1, false),
+      ('Dostawa 02-09',         '2023-02-09 00:00:00+00', 1, true),
+      ('Inwentaryzacja 02-10',  '2023-02-10 00:00:00+00', 1, false),
+      ('Dostawa 02-10',         '2023-02-10 00:00:00+00', 1, true),
+      ('Inwentaryzacja 02-11',  '2023-02-11 00:00:00+00', 1, false),
+      ('Dostawa 02-11',         '2023-02-11 00:00:00+00', 1, true);
 
 update public.product_record set price_per_unit = 19.0 where product_id = 1;
 update public.product_record set price_per_unit = 20.4 where id = 8;
 update public.product_record set price_per_unit = 21.0 where id = 15;
 update public.product_record set price_per_unit = 20.1 where id = 22;
 
+update public.product_record set quantity = 13.69 where product_id = 1;
+update public.product_record set quantity = 33.4 where id = 8;
+update public.product_record set quantity = 53.0 where id = 15;
+update public.product_record set quantity = 3.1 where id = 22;
+
 -- Recepty
 insert into public.recipe (name, company_id)
     values 
-      ('Sałatka owocowa', 1),
+      ('Sałatka mięsowa', 1),
       ('Spaghetti', 1);
 
 insert into public.recipe_part (quantity, recipe_id, product_id)
     values 
-      (0.2, 1, 3),
+      (0.2, 1, 1),
       (0.3, 1, 4),
-      (0.1, 1, 5),
-      (0.8, 2, 1);
+      (0.8, 2, 2),
+      (0.8, 2, 5);
