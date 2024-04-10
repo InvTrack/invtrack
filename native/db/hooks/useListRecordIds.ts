a41123c7ee649f70caa6d66617b7eaebc226a53c
Michal Struck <michal.o.struck@gmail.com>
2024-04-10 22:32:17 +0200

    app: fix useListRecordIds, memoize it

--- native/db/hooks/useListRecordIds.ts
+++ native/db/hooks/useListRecordIds.ts
@@ -1,9 +1,34 @@
-import { useListProductRecords } from "./useListProductRecords";
+import { useMemo } from "react";
+import { useListCategorizedProductRecords } from "./useListCategorizedProductRecords";
+import { useListUncategorizedProductRecords } from "./useListUncategorizedProductRecords";
 
-export const useListRecordIds = (inventoryId: number) => {
-  const { data: recordList, ...rest } = useListProductRecords(inventoryId);
+const getIds = (
+  uncategorizedRecordList: ReturnType<
+    typeof useListUncategorizedProductRecords
+  >["data"],
+  categorizedRecordList: ReturnType<
+    typeof useListCategorizedProductRecords
+  >["data"]
+) =>
+  [
+    ...(uncategorizedRecordList?.map(
+      (uncategorizedRecord) => uncategorizedRecord?.id
+    ) || []),
+    ...(categorizedRecordList?.flatMap((category) =>
+      category.data.map((record) => record?.id)
+    ) || []),
+  ].filter((id) => id != null) as number[];
+
+export const useListRecordIds = (inventoryId: number): { data: number[] } => {
+  const { data: uncategorizedRecordList } =
+    useListUncategorizedProductRecords(inventoryId);
+  const { data: categorizedRecordList } =
+    useListCategorizedProductRecords(inventoryId);
+  const ids = useMemo(
+    () => getIds(uncategorizedRecordList, categorizedRecordList),
+    [uncategorizedRecordList, categorizedRecordList]
+  );
   return {
-    data: recordList?.map((record) => record.id),
-    ...rest,
+    data: ids,
   };
 };
