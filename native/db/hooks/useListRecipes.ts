74d8ced1c58ff6246c2f887827a1770704611e70
Michal Struck <michal.o.struck@gmail.com>
2024-04-11 23:14:45 +0200

    app: sort recipes alphabetically

--- native/db/hooks/useListRecipes.ts
+++ native/db/hooks/useListRecipes.ts
@@ -5,8 +5,8 @@ import { supabase } from "../supabase";
 const listRecipes = async () => {
   const { data, error } = await supabase
     .from("recipe")
-    .select("id, name, recipe_part(id, quantity, product_id)")
-    .order("id", { ascending: true });
+    .select("id, name, recipe_part(quantity, product_id)")
+    .order("name", { ascending: true });
   if (error) throw new Error(error.message);
   return data;
 };
