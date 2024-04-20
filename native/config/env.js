a1b026b522d113fb5d6cf45427674c9372d879b7
Michal Struck <michal.o.struck@gmail.com>
2024-04-20 17:29:35 +0200

    app: bump version to 1.4.1

--- native/config/env.js
+++ native/config/env.js
@@ -6,7 +6,7 @@ import { channel } from "expo-updates";
 // the end user receives the correct key/url
 // https://docs.expo.dev/build-reference/variables/
 
-const runtimeVersion = "1.4.0";
+const runtimeVersion = "1.4.1";
 
 const isDevEnv =
   (process.env?.EXPO_ENV || process.env.NODE_ENV) === "development";
