1cdb4b6ee797f143c4e0b4a66e431b24b8562e9e
Felix Lipski <felix.lipski7@gmail.com>
2024-04-22 15:32:27 +0200

    fix doubled session context

--- native/db/auth/SessionContext.tsx
+++ native/db/auth/SessionContext.tsx
@@ -1,25 +1,16 @@
 import { Session } from "@supabase/supabase-js";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { supabase } from "../supabase";
-import { useGetTokens } from "./LinkingContext";
 
 type SessionContextLoggedInType = {
   session: Session;
   loggedIn: true;
-  companyId: number;
-  googleAccessToken: null;
 };
 type SessionContextNotLoggedInType = {
   session: null;
   loggedIn: false;
-  companyId: null;
-  googleAccessToken: null | string;
 };
-// TODO try to move to react-query
-/**
- * Session Context for user authentication, login etc.
- */
 type SessionContextType =
   | SessionContextLoggedInType
   | SessionContextNotLoggedInType;
@@ -27,55 +18,22 @@ type SessionContextType =
 export const SessionContext = createContext<SessionContextType>({
   loggedIn: false,
   session: null,
-  companyId: null,
-  googleAccessToken: null,
 });
 
-export const useSession = () => {
+export const useSessionState = () => {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
-  const [companyId, setCompanyId] = useState<null | number>(null);
-  const [googleAccessToken, setGoogleAccessToken] = useState<null | string>(
-    null
-  );
-  useGetTokens(setGoogleAccessToken);
-  useEffect(() => {
-    // TODO: remove duplicated code
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      // console.log("get session", session);
-      setSession(session);
-      if (session) {
-        return supabase
-          .from("worker")
-          .select("id, company_id")
-          .eq("id", session.user.id)
-          .single()
-          .then((res) => {
-            if (res && res.data) {
-              setCompanyId(res.data["company_id"]);
-            }
-          });
-      }
-      return null;
-    });
 
-    supabase.auth.onAuthStateChange(async (_event, session) => {
-      // console.log("auth changed", session);
-      setSession(session);
-      if (session) {
-        return supabase
-          .from("worker")
-          .select("id, company_id")
-          .eq("id", session.user.id)
-          .single()
-          .then((res) => {
-            if (res && res.data) {
-              setCompanyId(res.data["company_id"]);
-            }
-          });
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange(
+      async (_event, _session) => {
+        // console.log("auth changed", session?.user.id);
+        setSession(_session);
       }
-    });
+    );
     setLoading(false);
+
+    return () => data.subscription.unsubscribe();
   }, []);
 
   if (session && session.user) {
@@ -83,8 +41,6 @@ export const useSession = () => {
       session,
       loggedIn: true,
       loading,
-      companyId,
-      googleAccessToken,
     } as SessionContextLoggedInType & { loading: boolean };
   }
 
@@ -92,7 +48,10 @@ export const useSession = () => {
     session: null,
     loggedIn: false,
     loading,
-    companyId,
-    googleAccessToken,
   } as SessionContextNotLoggedInType & { loading: boolean };
+};
+
+export const useSession = () => {
+  const data = useContext(SessionContext);
+  return data;
 };
