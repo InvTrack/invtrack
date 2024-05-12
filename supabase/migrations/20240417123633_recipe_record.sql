6c2ce81ea5d663f6aee2f84ad6aa313ad7ad87eb
Michal Struck <michal.o.struck@gmail.com>
2024-05-12 23:00:06 +0200

    fix migration

--- supabase/migrations/20240417123633_recipe_record.sql
+++ supabase/migrations/20240417123633_recipe_record.sql
@@ -3,7 +3,8 @@ create table "public"."recipe_record" (
     "created_at" timestamp with time zone not null default now(),
     "quantity" numeric not null,
     "recipe_id" bigint not null,
-    "inventory_id" bigint not null
+    "inventory_id" bigint not null,
+    "company_id" bigint not null
 );
 
 alter table "public"."recipe_record" enable row level security;
@@ -20,12 +21,55 @@ alter table "public"."recipe_record" add constraint "recipe_record_recipe_id_fke
 
 alter table "public"."recipe_record" validate constraint "recipe_record_recipe_id_fkey";
 
-CREATE POLICY "Admin can do whatever, within company"
-ON public.recipe_record 
+alter table "public"."recipe_record" add constraint "recipe_record_company_id_fkey" FOREIGN KEY (company_id) REFERENCES company(id) not valid;
+
+alter table "public"."recipe_record" validate constraint "recipe_record_company_id_fkey";
+
+insert into recipe_record (inventory_id, recipe_id, company_id, quantity)
+   (select i.id as inventory_id, r.id as recipe_id, r.company_id, 0 as quantity 
+    from inventory i 
+    left join recipe r on r.company_id = i.company_id 
+    order by i.id asc);
+
+create policy "Admin can do anything within company"
+on "public"."recipe_record"
 as permissive
 for all
 to authenticated
-USING (( SELECT worker_for_current_user.is_admin
-   FROM worker_for_current_user) AND (EXISTS ( SELECT 1 FROM public.recipe r WHERE (
-        (r.company_id = ( SELECT current_company_id.id FROM public.current_company_id)) AND 
-        (r.id = recipe_record.recipe_id)))));
+using ((( SELECT worker_for_current_user.is_admin
+   FROM worker_for_current_user) AND ( SELECT (worker_for_current_user.company_id = recipe_record.company_id)
+   FROM worker_for_current_user)));
+
+
+CREATE POLICY "Non-admin can select on recipe_record, within company"
+ON "public"."recipe_record"
+AS PERMISSIVE
+FOR SELECT
+TO authenticated
+USING (
+  (SELECT (worker_for_current_user.company_id = recipe_record.company_id)
+   FROM worker_for_current_user)
+);
+
+CREATE POLICY "Non-admin can insert on recipe_record, within company"
+ON "public"."recipe_record"
+AS PERMISSIVE
+FOR INSERT
+TO authenticated
+WITH CHECK (
+  (SELECT (worker_for_current_user.company_id = recipe_record.company_id)
+   FROM worker_for_current_user)
+);
+
+CREATE POLICY "Non-admin can update on recipe_record, within company"
+ON "public"."recipe_record"
+AS PERMISSIVE
+FOR UPDATE
+TO authenticated
+USING (
+  (SELECT (worker_for_current_user.company_id = recipe_record.company_id)
+   FROM worker_for_current_user)
+);
+
+GRANT ALL PRIVILEGES ON TABLE "public"."recipe_record" TO "authenticated";
+GRANT ALL PRIVILEGES ON TABLE "public"."recipe_record" TO "service_role";
