0bb8eb123413f6449135d2e597368407b25fc97a
Konrad Struck <konrstruck@gmail.com>
2024-06-17 19:17:21 +0200

    admin: fixed product order in recipe edit and add dropdowns (#204)

--- admin/src/routes/(authenticated)/recipes/add/+page.ts
+++ admin/src/routes/(authenticated)/recipes/add/+page.ts
@@ -1,18 +1,21 @@
 import { error } from "@sveltejs/kit";
-export const load = async ({ parent, params }) => {
-  const id = params.id;
+export const load = async ({ parent }) => {
   const { supabase } = await parent();
 
-  const { data: products, error: supabaseError2 } = await supabase
-    .from("product")
-    .select();
+  const { data: products, error: supabaseError2 } = await supabase.from("product").select();
 
   if (supabaseError2) {
     console.error(supabaseError2);
     throw error(404, "Products not found.");
   }
+  const sortedProducts = products.sort((a, b) => {
+    const x = a.name.toLowerCase();
+    const y = b.name.toLowerCase();
+    return x.localeCompare(y);
+  });
+
   return {
     supabase,
-    products
+    products: sortedProducts,
   };
 };
