f56824f2ce286c51e8c789f2dd108e963988ed5a
Konrad Struck <konrstruck@gmail.com>
2024-06-18 17:04:44 +0200

    corrected equality operators

--- admin/src/routes/(authenticated)/recipes/Parts.svelte
+++ admin/src/routes/(authenticated)/recipes/Parts.svelte
@@ -33,8 +33,8 @@
     .map((p) => ({ name: p.name, value: p.id }))
     .filter(
       (p) =>
-        !newParts.find((newPart) => newPart.product_id == p.value) ||
-        deleteParts.find((deletedPart) => deletedPart.product_id == p.value)
+        !newParts.find((newPart) => newPart.product_id === p.value) ||
+        deleteParts.find((deletedPart) => deletedPart.product_id === p.value)
     );
   export let selectedProductId = null as number | null;
   export let quantity = null as number | null;
