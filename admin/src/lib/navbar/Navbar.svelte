3f7f9b5548fdf7f0eef54cd03e5a4d92c01cdaea
Konrad Struck <konrstruck@gmail.com>
2024-06-21 20:11:46 +0200

    Created a beautiful navbar for mobile

---
+++ admin/src/lib/navbar/Navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Navbar, NavHamburger, NavBrand } from "flowbite-svelte";
+  import logo_dark from "$lib/assets/logo-dark.png";
+  import logo_light from "$lib/assets/logo-light.png";
+  let isThemeDark = true;
+  export let hideSidebar = true;
+  const toggleSidebar = () => {
+    hideSidebar = !hideSidebar;
+  };
+</script>
+
+<Navbar class="md:hidden">
+  <NavHamburger on:click={() => toggleSidebar()} />
+  <NavBrand href="/" class="w-1/3 place-self-center">
+    {#if isThemeDark}
+      <img src={logo_dark} alt="InvTrack" />
+    {:else}
+      <img src={logo_light} alt="InvTrack" />
+    {/if}
+  </NavBrand>
+  <div />
+</Navbar>
