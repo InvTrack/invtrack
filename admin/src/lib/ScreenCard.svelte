2b3f7d39cd2608ed5ada5168566e8ebec77ca764
Konrad Struck <konrstruck@gmail.com>
2024-06-22 17:29:54 +0200

    removed unnecessary statement

--- admin/src/lib/ScreenCard.svelte
+++ admin/src/lib/ScreenCard.svelte
@@ -2,14 +2,11 @@
   import { Heading } from "flowbite-svelte";
   export let header: string | undefined = "";
 </script>
-{#if !header}
-<div class={"dark:bg-gray-800 bg-white rounded-lg mx-8 mt-4 p-8 " + $$props.class}>
-  <slot />
-</div>
-{:else}
-<Heading tag="h2" class="px-8 pt-4 text-gray-900 dark:text-white">{header}</Heading>
+
+{#if header}
+  <Heading tag="h2" class="px-8 pt-4 text-gray-900 dark:text-white">{header}</Heading>
+{/if}
 <!-- remember to keep a space after the class string -->
 <div class={"dark:bg-gray-800 bg-white rounded-lg mx-8 mt-4 p-8 " + $$props.class}>
   <slot />
 </div>
-{/if}
