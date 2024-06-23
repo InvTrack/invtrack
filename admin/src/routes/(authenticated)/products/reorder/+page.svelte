23c2724e9faf3a7f8356a0f80875909c1b9d3529
Michal Struck <michal.o.struck@gmail.com>
2024-06-23 22:26:36 +0200

    admin: improve dnd order editor, add tooltip, remove redundant deps, bump svelte-dnd-action

--- admin/src/routes/(authenticated)/products/reorder/+page.svelte
+++ admin/src/routes/(authenticated)/products/reorder/+page.svelte
@@ -2,10 +2,12 @@
   import ScreenCard from "$lib/ScreenCard.svelte";
   import { genericUpdate } from "$lib/genericUpdate.js";
   import { currentCompanyId } from "$lib/store.js";
-  import { Button } from "flowbite-svelte";
+  import { Button, Span } from "flowbite-svelte";
   import Columns from "./Columns.svelte";
   import BareItems from "./BareItems.svelte";
   import UnsavedWarningModal from "$lib/modals/UnsavedWarningModal.svelte";
+  import Tooltip from "$lib/Tooltip.svelte";
+  import { BarsOutline } from "flowbite-svelte-icons";
 
   export let data;
 
@@ -69,11 +71,40 @@
   const setUnsavedChanges = () => (unsavedChanges = true);
 </script>
 
-<ScreenCard header="Produkty">
+<ScreenCard header="Produkty" class="flex flex-col gap-2">
+  <Span class="flex flex-row self-end"
+    >Jak używać edytora? <Tooltip id="products-dnd-ttip">
+      <div class="p-3">
+        <h3 class="font-semibold text-gray-900 dark:text-white">
+          Edytor drag'n'drop - jak używać?
+        </h3>
+        <div class="inline">
+          W InvTrack, możliwe jest ułożenie produktów i kategorii w ustalonej przez siebie
+          kolejności. Dodatkowo, produkty mogą zawierać się w kategorii, również w ustalonej
+          kolejności.
+          <br />
+          - Aby przesunąć produkt, złap go za ikonę <BarsOutline class="w-2 inline " /> i przesuń w dowolne
+          miejsce w obrębie kategorii, lub do obszaru bez kategorii (na samej górze ekranu).
+          <br />
+          To samo zrób z kategorią, by zmienić jej kolejność.
+          <br />
+          - Kategorie mogą zawierać dużo produktów, <u>autoprzewijanie (scroll)</u> działa przy
+          zblieniu się z przesuwanym produktem do górnej lub dolnej granicy kategorii.
+          <br />
+        </div>
+        <u>Numery przed nazwą kategorii nie wyświetlają się w aplikacji mobilnej.</u>
+        <br />
+        <strong class="text-gray-900 dark:text-white">
+          Odpowiednia kolejność wyjątkowo pomaga w aplikacji. Proponujemy ustalić kolejność zgodnie
+          z występowaniem w magazynie, lub po dostawcach.
+        </strong>
+      </div>
+    </Tooltip></Span
+  >
   <UnsavedWarningModal bind:unsavedChanges />
   <BareItems itemsContainer={uncategorisedProductsContainer} {setUnsavedChanges} />
   <Columns columnsContainer={categoriesContainer} {setUnsavedChanges} />
-  <Button class="mt-4" color="primary" on:click={update}
+  <Button class="self-start" color="primary" on:click={update}
     >{loading ? "Zapisywanie..." : "Aktualizuj kolejność"}</Button
   >
 </ScreenCard>
