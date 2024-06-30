d2c1d3bf8aa3b9968be33f7a4e8fff1cf1da0982
Michal Struck <michal.o.struck@gmail.com>
2024-06-30 12:57:00 +0200

    remove python script

--- gen-random-data.py
+++
@@ -1,9 +0,0 @@
-import random
-import math
-
-for inventory_id in range(10):
-    for product_id in range(4):
-        num = random.random() * 100
-        if product_id == 0:
-            num = math.floor(num)
-        print("(" + str(inventory_id + 1) + ", " + str(product_id + 1) + ", " + str(num)[:4] + "),")
